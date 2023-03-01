512c598ff3c9903bc52478dacc8fbdfd9d0800b1
LAP__pampers <lappampers@gmail.com>
2023-03-01 17:53:53 +0200

    first commit

---
+++ conditions.py
@@ -0,0 +1,8 @@
+age=input("enter your age")
+age=int(age)
+# if age>= 14:
+#     print("your are older than 14 or you are 14")
+# if age <=14:
+#     print("your are younger 14 you are 14")
+# if age != 14:
+#     print("you are not 14")

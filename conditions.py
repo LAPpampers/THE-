8cc30542917dceb3dc5338fb0b7651df2b012502
LAP__pampers <lappampers@gmail.com>
2023-03-15 17:10:04 +0200

    first commit

--- conditions.py
+++ conditions.py
@@ -1,8 +1,36 @@
-age=input("enter your age")
-age=int(age)
+# age=input("enter your age")
+# age=int(age)
 # if age>= 14:
 #     print("your are older than 14 or you are 14")
 # if age <=14:
 #     print("your are younger 14 you are 14")
 # if age != 14:
 #     print("you are not 14")
+# homework for nemetskiy tarakan
+# Добавить в программу ввод длины основания и 
+# высоты треугольника и произвести расчет площади треугольника по формуле: S=(b*h)/2 , где S - площадь треугольника; b - сторона-основание треугольника; h - высота треугольник
+# b=input("enter the base triangle")
+# b=int(b)
+# h=input("enter the height of the triangle")
+# h=int(h)
+# s=(b*h)/2
+# print(f"the area triangle is {s}")
+# if 1<= age <=12: 
+#     print("you are kid")
+# if 13<= age <=19:
+#     print("you are teenager")
+# if "word" == "word":
+#     print("it the same word")
+# if 5 == "5":
+#     print("amiricle happened"
+# things=" they are german cockroach shpanish cockroach bottle and pen on the table"
+# if "german cockroach" in things: 
+#     print("the german cockroach has been found")
+# if "tatar cockroach" not in things:
+#     print("the tatar cockroach has not be found")
+number=input("enter your number")
+number=int(number)
+if number %2==0:
+    print("even number")
+else:
+    print("odd number")
---
+++ what is the sesson.py
@@ -0,0 +1,10 @@
+month=input("enter your number")
+month=int(month)
+if 1<= month <=2 or month==12:
+    print("your month is winter")
+if 3<= month <=5:
+    print("your month is spring")
+if 6<= month <=8:
+    print("your month is summer")
+if 9<= month <=11:
+    print("your month is autumn")
